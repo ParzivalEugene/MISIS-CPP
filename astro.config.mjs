6c8ac4c71a6ebc1737a7e903d1ca4d38ed768a1e
Eugene Michkov <michkov.evgeny@gmail.com>
2024-06-11 22:18:47 +0300

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,20 @@
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "My Docs",
+      social: {
+        github: "https://github.com/withastro/starlight",
+      },
+      sidebar: [
+        {
+          label: "Вопросы",
+          autogenerate: { directory: "questions" },
+        },
+      ],
+    }),
+  ],
+});
